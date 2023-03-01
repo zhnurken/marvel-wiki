c0ea881796b179ae9d65ff7e60bffabab61a2ac8
Nurken Zhappargaly <zhnurken@gmail.com>
2023-03-01 12:29:25 +0600

    add React Transition Group

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,8 +1,10 @@
 import {useState, useEffect, useRef} from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import useMarvelService from '../../services/MarvelServices';
+
 import './charList.scss';
 
 const CharList = (props) => {
@@ -54,7 +56,8 @@ const CharList = (props) => {
             }
             
             return (
-                <li 
+                <CSSTransition key={item.id} timeout={500} classNames="char__item">
+                    <li 
                     className="char__item"
                     tabIndex={0}
                     ref={el => itemRefs.current[i] = el}
@@ -72,12 +75,15 @@ const CharList = (props) => {
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
+                </CSSTransition>
             )
         });
 
         return (
             <ul className="char__grid">
-                {items}
+                <TransitionGroup component={null}>
+                    {items}
+                </TransitionGroup>
             </ul>
         )
     }
--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react';
 import useMarvelService from '../../services/MarvelServices';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
+import AppBanner from '../appBanner/AppBanner';
 
 const SingleComicPage = () => {
     const {comicId} = useParams();
@@ -17,13 +18,13 @@ const SingleComicPage = () => {
     }, [comicId])
 
     const updateComic = () => {
-    clearError();
-    getComic(comicId)
-        .then(onComicLoaded)
+        clearError();
+        getComic(comicId)
+            .then(onComicLoaded)
     }
 
     const onComicLoaded = (comic) => {
-    setComic(comic);
+        setComic(comic);
     }
     const errorMessage = error ? <ErrorMessage/> : null;
     const spinner = loading ? <Spinner/> : null;
@@ -31,6 +32,7 @@ const SingleComicPage = () => {
 
     return (
         <>
+            <AppBanner/>
             {errorMessage}
             {spinner}
             {content}
@@ -43,17 +45,17 @@ const SingleComicPage = () => {
 
 
         return (
-        <div className="single-comic">
-            <img src={thumbnail} alt={title} className="single-comic__img"/>
-            <div className="single-comic__info">
-                <h2 className="single-comic__name">{title}</h2>
-                <p className="single-comic__descr">{description}</p>
-                <p className="single-comic__descr">{pageCount}</p>
-                <p className="single-comic__descr">Language: {language}</p>
-                <div className="single-comic__price">{price}</div>
+            <div className="single-comic">
+                <img src={thumbnail} alt={title} className="single-comic__img"/>
+                <div className="single-comic__info">
+                    <h2 className="single-comic__name">{title}</h2>
+                    <p className="single-comic__descr">{description}</p>
+                    <p className="single-comic__descr">{pageCount}</p>
+                    <p className="single-comic__descr">Language: {language}</p>
+                    <div className="single-comic__price">{price}</div>
+                </div>
+                <Link to="/comics" className="single-comic__back">Back to all</Link>
             </div>
-            <Link to="/comics" className="single-comic__back">Back to all</Link>
-        </div>
     )
 }
 
