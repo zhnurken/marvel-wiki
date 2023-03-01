d147d86817a1a54f56e1d541cacca690e4401e7f
Nurken Zhappargaly <zhnurken@gmail.com>
2023-03-01 18:11:13 +0600

    add CharSearchForm, SingleCharacterLayout

--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -11,22 +11,27 @@ const useMarvelService = () => {
     const getAllCharacters = async (offset = _baseOffset) => {
         const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformCharacter)
-    }
+    };
+
+	const getCharacterByName = async (name) => {
+		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+		return res.data.results.map(_transformCharacter);
+	};
 
     const getCharacter = async (id) => {
         const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
         return _transformCharacter(res.data.results[0]);
-    }
+    };
 
     const getAllComics = async (offset = 0) => {
         const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformComics)
-    }
+    };
     
     const getComic = async (id) => {
         const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
         return _transformComics(res.data.results[0]);
-    }
+    };
 
     const _transformCharacter = (char) => {
         return {
@@ -38,7 +43,8 @@ const useMarvelService = () => {
             wiki: char.urls[1].url,
             comics: char.comics.items
         }
-    }
+    };
+
     const _transformComics = (comics) => {
         return {
             id: comics.id,
@@ -52,10 +58,19 @@ const useMarvelService = () => {
 			price: comics.prices[0].price
 				? `${comics.prices[0].price}$`
 				: "not available",
-        }
-    }
+        };
+    };
 
-    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic}
-}
+    return {
+            loading, 
+            error, 
+            clearError, 
+            getAllCharacters, 
+            getCharacterByName,
+            getCharacter,
+            getAllComics, 
+            getComic
+        };
+};
 
 export default useMarvelService;
